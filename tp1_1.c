f7339ac3020e074236948f0a27d96afef309e78b
PabloEzequielVillagran <ezequielvillagran333@gmail.com>
2023-03-18 17:30:38 -0300

    punto3 apartado e

--- tp1_1.c
+++ tp1_1.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 int main(){
 
 printf("hola mundo\n");
 
+int *punt;
 
+int f = 3;
+
+punt = &f;
+
+//punt = malloc(f*sizeof(int));
+
+printf("El contenido del puntero es %d\n", *punt);
+printf("La direccion almacenada por el puntero es %d\n", punt);
+printf("La direccion de memoria de la variable es %d\n", &f);
+printf("La direccion de memoria del puntero es %d\n", &punt);
+printf("El tamaño de memoria utilizado por la variable %d es %d\n", f, sizeof(f));
 
     return 0;
 }
